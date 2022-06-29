572614e8686bc017d4798d678fd4ba921d0304f5
pransin <pranjal10singhal@gmail.com>
2022-06-29 19:22:36 +0530

    styled login page and home page

--- website/datab.py
+++ website/datab.py
@@ -1,24 +1,26 @@
 import mysql.connector as c
+import os
+
 
 def connect():
     try:
         mydb = c.connect(
-                host="localhost",
-                port="3306",
-                user="root",
-                passwd="vatsal",
-                database="dbs_vi"
-            )
+            host="localhost",
+            port="3306",
+            user="root",
+            passwd=os.environ.get("DBS_VI_PASSWORD", "vatsal"),
+            database="dbs_vi"
+        )
         return mydb
-    except:
+    except Exception as exec:
         print("Connection couldn't be established")
+        print(exec)
         exit()
-        return None
+
 
 def s_login(mydb, ID, passw):
-    cur=mydb.cursor()
-    
-    
+    cur = mydb.cursor()
+
     # cur=mydb.cursor()
     # cur.execute('''SHOW tables''')
     # result = cur.fetchall()
