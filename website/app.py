54ec84b0c1b2c8ac3ba90cee378d8b28bf7c5da6
vatsal-pattani <vatsal.ar697@gmail.com>
2022-07-15 17:15:38 +0530

    create paper is fully functional now

--- website/app.py
+++ website/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from .users import *
 from dotenv import load_dotenv
 
@@ -103,17 +103,6 @@ def questions():
     questions = execute(
         f'''select marks, Question, Opt1, Opt2, Opt3, Opt4, Q_id, correct_ans from Questions where (questions.E_id = '{session['E_id']}') and (questions.C_id = '{session['c_id']}'); -- Displays all questions of that test ''')
 
-    # We can calculate total marks of exam here. we actually fetched it from database
-    # But the snippet below will be useful when IC creates a paper
-
-    # total_marks=0
-    # i=0
-    # while(i<len(qid_list)):
-    #     total_marks=total_marks+float(questions[i][0])
-    #     i=i+1
-    # session['total_marks']=total_marks
-
-    # Not passing correct ans and Q_id to HTML as they are not needed
     return render_template("questions.html", questions=questions, tm=total_marks)
 
 
@@ -237,43 +226,60 @@ def IC_dashboard():
 
 @app.route('/view_paper')
 def view_paper():
-    E_id = request.form.get('E_id')
+    E_id = request.args.get('E_id')
+    print(request.args)
+    # session['E_id'] = E_id
+
+    print(E_id)
+    print(session['c_id'])
     session['E_id'] = E_id
-    subject = session['subject']
-    c_id = execute(
-        f'''select C_id from course where (title = '{subject}'); ''')
-    session['c_id'] = c_id[0][0]
-
-    total_marks = execute(
-        f'''select total_marks from exam where (exam.e_id = '{E_id}') and (exam.c_id = '{session['c_id']}');''')[0][0]
-    session['total_marks'] = total_marks
-
     questions = execute(
-        f'''select marks, Question, Opt1, Opt2, Opt3, Opt4, Q_id, correct_ans from Questions where (questions.E_id = '{session['E_id']}') and (questions.C_id = '{session['c_id']}'); -- Displays all questions of that test ''')
+        f'''select marks, Question, Opt1, Opt2, Opt3, Opt4, Q_id, correct_ans from Questions where (questions.E_id = '{E_id}') and (questions.C_id = '{session['c_id']}'); -- Displays all questions of that test ''')
 
+    total_marks = execute(f'''select total_marks from Exam
+                              where E_id='{E_id}' and C_id='{session['c_id']}' ''')[0][0]
+    session['total_marks'] = total_marks
+    print(total_marks)
     return render_template("questions_IC.html", questions=questions, tm=total_marks)
+    
 
 
 @app.route('/create_paper', methods=['GET', 'POST'])
 def create_paper():
-
+# First we will have to generate new E_id for this c_id, we'll have to refer to db for that
+    last_eid = execute(f'''select E_id from exam
+                where exam.c_id = '{session['c_id']}'
+                order by E_id desc
+                limit 1''')[0][0]
+    new_eid = "e" + str(int(last_eid[1:]) + 1).zfill(5)   # Creating a new e_id
+    session['new_eid'] = new_eid
+    
     return render_template("create_paper.html", session=session)
 
 
-@app.route('/add_paper', methods=['GET', 'POST'])
-def add_paper():  # This will add paper to db
+@app.route('/add_question', methods=['POST'])
+def add_question():  # This will add paper to db
+    print("We are here!")
+    # print(request.form)
+    form = request.form
+    last_qid = execute(f'''select Q_id from Questions
+                order by Q_id desc
+                limit 1''')[0][0]
+    new_qid = "q" + str(int(last_qid[1:]) + 1).zfill(5)   # Creating a new e_id
+    print(new_qid)
+    execute(f'''insert into Questions(Q_id,marks,Question,Opt1,Opt2,Opt3,Opt4,correct_ans,E_id,C_id)
+                values ('{new_qid}',{int(form['marks'])},'{form['q']}', '{form['A']}','{form['B']}','{form['C']}','{form['D']}','{form['c_ans']}','{session['new_eid']}','{session['c_id']}'); ''')
+    return "Question added successfully"
+
+@app.route('/add_exam', methods=['POST'])
+def add_exam():
+    # print("We're in add exam!")
     print(request.form)
-
-    # First we will have to generate new E_id for this c_id, we'll have to refer to db for that
-    # last_eid = execute(f'''select E_id from exam
-    #             where exam.c_id = '{session['c_id']}'
-    #             order by E_id desc
-    #             limit 1''')[0][0]
-    # new_eid = "e" + str(int(last_eid[1:]) + 1).zfill(5)   # Creating a new e_id
-
-    # request.form
-
-    return "Paper added successfully"
+    form = request.form
+    execute(f'''insert into Exam(E_id, C_id, duration, exam_time, total_marks)
+                values ('{session['new_eid']}','{session['c_id']}','{form['duration']}','{form['timing']}',{int(form['total_marks'])}) ''')
+    flash('Paper Added Successfully', 'success')
+    return redirect(url_for('IC_dashboard'))
 
 
 @app.route('/edit_paper/<E_id>')
